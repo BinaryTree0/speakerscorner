723940b75e0fb87b523863b781ad7f265f2d5fa0
Filip <filip.radovic@fer.hr>
2019-01-04 16:32:45 +0100

    Dodan dynamic domain

--- konferencija/users/utils.py
+++ konferencija/users/utils.py
@@ -2,6 +2,7 @@ from django import utils
 from konferencija.settings import EMAIL_HOST
 from django.core.mail import send_mail
 from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.models import Site
 
 def send_mail_to(instance):
     #Security mesures creation of custom token and uid
@@ -10,5 +11,7 @@ def send_mail_to(instance):
     uid = uid.decode("utf-8")
     #Sending mail
     from_email = EMAIL_HOST
-    message = 'http://127.0.0.1:8000/users/validate/'+ uid + '/' + token +'/'
+    current_site = Site.objects.get_current()
+    current_site.domain
+    message = 'http://' + current_site.domain  + '/users/validate/'+ uid + '/' + token +'/'
     send_mail('Registration',message,from_email,[instance.email],fail_silently=False,)
