134b58f01f0f9eeb0d7acd7708a9c91deb4dc9de
Filip <filip.radovic@fer.hr>
2019-01-13 22:18:42 +0100

    Update

--- konferencija/users/models.py
+++ konferencija/users/models.py
@@ -4,7 +4,6 @@ from django.db import models
 from django.urls import reverse_lazy
 #Local imports
 from . import utils
-
 #Custom user that has email instead as username field
 class CustomUser(AbstractUser):
     email = models.EmailField(('email address'), unique=True)
